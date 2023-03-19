a0993b49f818de35d2cb1fe623d9dae04228f22e
Malek Jerbi <malek.jerbi@gmail.com>
2023-03-19 19:33:18 -0400

    idea generator

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,62 +1,52 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+})
+const openai = new OpenAIApi(configuration)
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
+        message:
+          'OpenAI API key not configured, please follow instructions in README.md',
+      },
+    })
+    return
   }
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      model: 'text-davinci-003',
+      prompt: generatePrompt(),
       temperature: 0.6,
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+      max_tokens: 350,
+      suffix: '7',
+    })
+    res.status(200).json({ result: completion.data.choices[0].text })
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+      console.error(error.response.status, error.response.data)
+      res.status(error.response.status).json(error.response.data)
     } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
+      console.error(`Error with OpenAI API request: ${error.message}`)
       res.status(500).json({
         error: {
           message: 'An error occurred during your request.',
-        }
-      });
+        },
+      })
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt() {
+  return `suggest only one more startup idea for people who have background of programming and computer science.
+  1- An AI-driven platform for online tutoring and education, with personalized learning programs tailored to each student's needs. 
+  2- A software development service that creates custom applications for businesses in need of automation or improved efficiency. 
+  3- A cloud-based data storage and sharing platform designed specifically for medical professionals and researchers to securely store patient records and share research findings more easily among colleagues globally. 
+  4- A blockchain-powered marketplace that connects freelancers with employers looking for short-term projects requiring advanced programming skillsets, such as developing smart contracts or creating decentralized applications (DApps). 
+  5- An artificial intelligence (AI)-driven search engine which uses natural language processing (NLP) algorithms to match job seekers.
+  6-`
 }
